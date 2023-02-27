362cfa5869cacf0994197d75c8c6b719a741ce8f
Heitor Queiroz <heitor.qf@hotmail.com>
2023-02-27 14:31:26 -0300

    Primeiro commit - Arquivos do projeto

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "../template/Main"
+
+
+export default props =>
+    <Main icon="home" title="Inicio"
+        subtitle="Segundo Projeto">
+
+        <div className="display-4">Bem Vindo</div>
+        <hr />
+        <p className="mb-0">Exercício de cadastro com React</p>
+    </Main>
